d3429fb4990dd3165ec3ad4693f7111f6bbc9c09
root <root@instance-2.europe-west4-a.c.astral-236111.internal>
2019-04-02 12:08:07 +0000

    dir path to unix

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 //app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static("C:/Sync/db/public"));
+app.use(express.static("/home/Desktop/music"));
 app.use('/music', musicRouter);
 
 
